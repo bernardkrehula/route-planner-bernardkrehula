5b6df621e6052d84253fb17c6b03c11391d02914
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-29 22:41:19 +0200

    feat: add <MarkerCluster>, <Marker> and <PopUp> component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
 import "./App.css";
 import Btn from "#/components/ui/btn";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+  Popup,
+  useMapEvent,
+  useMapEvents,
+} from "react-leaflet";
+import { Icon } from "leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 
 const App = () => {
+  const customIcon = new Icon({
+    iconUrl: "/location.png",
+    iconSize: [38, 38],
+  });
+  const markers = [
+    {
+      geocode: [46.3129033, 16.34],
+      popUp: "Welcome to Varazdin",
+    },
+    {
+      geocode: [46.34, 16.28],
+      popUp: "Welcome to Sracinec",
+    },
+  ];
   const tileLayerOptions = {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    attribution: "stamen open street",
+    url: "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.jpg",
   } as any;
 
   return (
     <div>
       <Btn></Btn>
       <h1>Radi</h1>
-      <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+      <MapContainer center={[46.3129033, 16.2650311]} zoom={13}>
+        {/*  <TileLayer {...tileLayerOptions} /> */}
         <TileLayer {...tileLayerOptions} />
+        <MarkerClusterGroup>
+          {markers.map((marker, key) => (
+            <Marker
+              key={key}
+              position={marker.geocode}
+              {...({ icon: customIcon } as any)}
+            >
+              <Popup>{marker.popUp}</Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
       </MapContainer>
     </div>
   );
