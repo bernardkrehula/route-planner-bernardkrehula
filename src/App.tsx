3d7ec73cab3380897482ca75f547702172acf2c4
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-31 20:46:28 +0200

    feat: add travelRoute useState to <App> component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import MapWrapper from "./components/map/MapWrapper";
 import RoutePannel from "./components/map/RoutePannel";
+import { requestRoute } from "./api/requestRoute";
+import { useNavigate } from "react-router";
+import { timeCalculator } from "./utils/timeCalculator";
+import { distanceCalculator } from "./utils/distanceCalculator";
 
-const App = () => {
+const App = ({ defaultCoords }) => {
+ 
+  const [travelRoute, setTravelRoute] = useState([[0, 0]]);
+  
+
   return (
     <div className="app">
-      <RoutePannel />
-      <MapWrapper />
+      <RoutePannel handleTravelRoute={handleTravelRoute} />
+      <MapWrapper
+        defaultCoords={defaultCoords}
+        travelRoute={travelRoute}
+        travelInfo={travelInfo}
+      />
     </div>
   );
 };
