7e67f68001e065b63e13cda3a50411c7a3187386
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-31 20:48:28 +0200

    feat: add travelOption prop to requestRoute function

--- src/api/requestRoute.ts
+++ src/api/requestRoute.ts
@@ -1,24 +1,16 @@
 import axios from "axios";
 
-export const requestRoute = async () => {
-  const cords = {
-    start: [46.334298, 16.273321],
-    end: [46.305979, 16.335594],
-  };
-  const startParam = `${cords.start[1]},${cords.start[0]}`;
-  const endParam = `${cords.end[1]},${cords.end[0]}`;
+export const requestRoute = async (cord, travelOption) => {
+  const startParam = `${cord[0][1]},${cord[0][0]}`;
+  const endParam = `${cord[1][1]},${cord[1][0]}`;
   const apiKey =
     "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjJiMmQ0ZTllYmNkNDQ2ZWVhYTZhOWE0Yzc5Mzc0N2FhIiwiaCI6Im11cm11cjY0In0";
   try {
     const response = await axios.get(
-      `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${apiKey}=&start=${startParam}&end=${endParam}`,
+      `https://api.openrouteservice.org/v2/directions/${travelOption}?api_key=${apiKey}=&start=${startParam}&end=${endParam}`,
     );
-    const rawCoordinates = response.data.features[0].geometry.coordinates;
-    const leafletReadyRoute = rawCoordinates.map(([lng, lat]) => [lat, lng]);
-    console.log(leafletReadyRoute)
-    return leafletReadyRoute;
+    return response.data.features[0];
   } catch (error) {
     console.error(error);
   }
 };
-
