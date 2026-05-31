1c21f68779794b643b3a05ad99414cf22d1183d9
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-31 19:38:30 +0200

    feat: add and pass defaultCoords as prop

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 
+const defaultCoords = [46.31, 16.34];
 
 const router = createBrowserRouter([
   {
-    element: <App />,
+    element: <App defaultCoords={defaultCoords}/>,
     path: "/"
   },
 ]);
