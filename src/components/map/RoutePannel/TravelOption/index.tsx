ad16fbc119b7885bf24a073a3ac9682704d01163
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-31 20:49:51 +0200

    feat: add handleTravelOptionSelect function to <TravelOption> component

--- src/components/map/RoutePannel/TravelOption/index.tsx
+++ src/components/map/RoutePannel/TravelOption/index.tsx
@@ -1,14 +1,27 @@
-import './index.css';
+import { useNavigate, useSearchParams } from "react-router";
+import "./index.css";
+import type React from "react";
 
 const TravelOption = () => {
-    return(
-        <div className="traveling-options-div">
-        <select name="traveling" className="traveling-options">
-          <option value='driving'>Driving 🚗</option>
-          <option value='walking'>Walking 🚶‍♂️</option>
-        </select>
-        <span>▼</span>
-      </div>
-    )
-}
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const handleTravelOptionSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const option = e.target.value;
+    const newParams = new URLSearchParams(searchParams);
+    newParams.set('travel', option);
+    navigate(`/?${newParams.toString()}`);
+  };
+
+  return (
+    <div className="traveling-options-div">
+      <select name="traveling" className="traveling-options" onChange={handleTravelOptionSelect}>
+        <option value="driving-car">Driving 🚗</option>
+        <option value="cycling-regular">Cycling 🚴</option>
+        <option value="foot-walking">Walking 🚶‍♂️</option>
+      </select>
+      <span>▼</span>
+    </div>
+  );
+};
 export default TravelOption;
