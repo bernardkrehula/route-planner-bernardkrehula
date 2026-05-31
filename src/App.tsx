eeed077af5689f89764dc4c84e3c92f325807aca
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-31 20:47:42 +0200

    feat: add handleTravelRoute function to <App> component

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,22 @@ const App = ({ defaultCoords }) => {
     duration: '0',
   });
 
- 
+  useEffect(() => {
+    navigate("");
+  }, []);
+
+  const handleTravelRoute = async (cords, travelOption) => {
+    const response = await requestRoute(cords, travelOption);
+    const rawCoordinates = response.geometry.coordinates;
+    const leafletReadyRoute = rawCoordinates.map(([lng, lat]) => [lat, lng]);
+    const info = response.properties.summary;
+    const duration = timeCalculator(info.duration)
+    const distance = distanceCalculator(info.distance);
+    const infoAboutTravel = {duration, distance}
+    console.log(duration)
+    setTravelInfo(infoAboutTravel);
+    setTravelRoute(leafletReadyRoute);
+  };
 
   return (
     <div className="app">
