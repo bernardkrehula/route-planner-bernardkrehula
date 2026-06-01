157d67ef2bf6d2076b97875035b243e9afdda64c
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-01 21:44:05 +0200

    refactor: change FormInputType properties

--- src/types/form.types.ts/FormInputType.ts
+++ src/types/form.types.ts/FormInputType.ts
@@ -1,4 +1,12 @@
+import type { InputValuesType } from "./InputValuesType";
+import type { GeoJsonFeature } from "../geo.types.ts/GeoJsonFeatureType";
+
 export type FormInputType = {
-    name: string;
-    placeholder: string;
-}
+  name: string;
+  placeholder: string;
+  inputValues: InputValuesType;
+  activeSearch: boolean;
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  searchedDestinations: GeoJsonFeature[];
+  handleDestinationClick: (value: string) => void;
+};
