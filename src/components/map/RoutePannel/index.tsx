8e5b64728370d4ad6035179a462d7adad20b21c4
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-01 16:59:05 +0200

    refactor: move InputValues useState to <RoutePannel>

--- src/components/map/RoutePannel/index.tsx
+++ src/components/map/RoutePannel/index.tsx
@@ -1,26 +1,22 @@
 import Btn from "#/components/ui/btn";
-import { useState, type FormEvent } from "react";
+import { useState } from "react";
 import "./index.css";
 import FormInput from "./FormInput";
-import { requestRoute } from "#/api/requestRoute";
 import TravelOption from "./TravelOption";
-import { useNavigate, useSearchParams } from "react-router";
 import { parseRouteCoordinates } from "#/utils/mapHelper";
 
-const RoutePannel = ({handleTravelRoute}) => {
+const RoutePannel = ({ handleTravelRoute, searchParams, navigate }) => {
   const [activeStop, setActiveStop] = useState<boolean>(false);
-  const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
+  const [inputValues, setInputValues] = useState({
+    start: "",
+    stop: "",
+    end: "",
+  });
 
-  const startParam = searchParams.get("start");
-
-  const handleCheckPoints = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    
+  const handleCheckPoints = () => {
     const startParam = searchParams.get("start");
     const endParams = searchParams.get("end");
-    const travelParams = searchParams.get("travel") || 'driving-car';
-    console.log(travelParams)
+    const travelParams = searchParams.get("travel") || "driving-car";
     const cords = parseRouteCoordinates(startParam, endParams);
     handleTravelRoute(cords, travelParams);
   };
@@ -30,31 +26,55 @@ const RoutePannel = ({handleTravelRoute}) => {
   };
   const clearRoute = () => {
     navigate("");
+    
   };
   return (
-    <form className="route-pannel" onSubmit={handleCheckPoints}>
+    <div className="route-pannel">
       <div className="title">
         <img src="./location.png" />
         <h1>Route planner</h1>
       </div>
       <div className="destination-inputs">
-        <FormInput name="start" placeholder="Start destination" />
-        {activeStop && <FormInput name="stop" placeholder="Stop" />}
-        <FormInput name="end" placeholder="Destination" />
+        <FormInput
+          name="start"
+          placeholder="Start destination"
+          searchParams={searchParams}
+          navigate={navigate}
+          setInputValues={setInputValues}
+          inputValues={inputValues}
+        />
+        {activeStop && (
+          <FormInput
+            name="stop"
+            placeholder="Stop"
+            searchParams={searchParams}
+            navigate={navigate}
+            setInputValues={setInputValues}
+            inputValues={inputValues}
+          />
+        )}
+        <FormInput
+          name="end"
+          placeholder="Destination"
+          searchParams={searchParams}
+          navigate={navigate}
+          setInputValues={setInputValues}
+          inputValues={inputValues}
+        />
       </div>
       <Btn type="button" onClick={addStopDestination} variation="secondary">
         {activeStop ? "Remove stop" : "Add stop"}
       </Btn>
       <TravelOption />
       <div className="routing-btns">
-        <Btn type="submit" variation="secondary">
+        <Btn type="button" onClick={handleCheckPoints} variation="secondary">
           Calcutate route
         </Btn>
         <Btn type="reset" onClick={clearRoute} variation="secondary">
           Clear route
         </Btn>
       </div>
-    </form>
+    </div>
   );
 };
 export default RoutePannel;
