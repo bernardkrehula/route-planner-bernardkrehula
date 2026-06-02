e04639c170a860af6b9880738ca9c4183ccabeb4
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-02 22:07:23 +0200

    refactor: pass values as props down to <FormInput> component and remove unused code

--- src/components/map/RoutePannel/FormInput/index.tsx
+++ src/components/map/RoutePannel/FormInput/index.tsx
@@ -1,85 +1,17 @@
-import React, { useState } from "react";
 import "./index.css";
 import Input from "#/components/ui/input";
-import { requestCordinates } from "#/api/requestCordinates";
-import type { GeoJsonFeature } from "#/types/geo.types.ts/GeoJsonFeatureType";
-import { debouncedFetch } from "#/utils/debounceFetch";
 import type { FormInputType } from "#/types/form.types.ts/FormInputType";
-import Btn from "#/components/ui/btn";
 import ListItem from "./ListItem";
 
 const FormInput = ({
   name,
   placeholder,
-  searchParams,
-  navigate,
-  setInputValues,
-  inputValues
+  inputValues,
+  activeSearch,
+  handleInputChange,
+  searchedDestinations,
+  handleDestinationClick
 }: FormInputType) => {
-  const [searchedDestinations, setSearchedDestinations] = useState<
-    GeoJsonFeature[]
-  >([
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [],
-      },
-      properties: {
-        id: "",
-        gid: "",
-        layer: "",
-        source: "",
-        source_id: "",
-        name: "",
-        confidence: null,
-        match_type: "",
-        accuracy: "",
-        country: "",
-        country_gid: "",
-        country_a: "",
-        region: "",
-        region_gid: "",
-        region_a: "",
-        county: "",
-        county_gid: "",
-        county_a: "",
-        continent: "",
-        continent_gid: "",
-        label: "",
-      },
-      bbox: [],
-    },
-  ]);
-  const [activeSearch, setActiveSearch] = useState<boolean>(false);
-
-  const handleSearchRequest = async (destination: string) => {
-    const response = await requestCordinates(destination);
-    setSearchedDestinations(response.features);
-  };
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setInputValues(prev => ({...prev, [name]: value}));
-    if (value.trim() != "") {
-      debouncedFetch(value, handleSearchRequest);
-      setActiveSearch(true);
-    } else {
-      setActiveSearch(false);
-      setSearchedDestinations([]);
-    }
-  };
-
-  const handleDestinationClick = async (destination: string) => {
-    setInputValues(prev => ({...prev, [name]: destination}));
-    setActiveSearch(false);
-    const response = await requestCordinates(destination);
-    const cords = response.features[0].geometry.coordinates;
-    const newParams = new URLSearchParams(searchParams);
-    newParams.set(name, cords);
-    navigate(`/?${newParams.toString()}`);
-  };
-
-  
 
   return (
     <form className="form-input">
