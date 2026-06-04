8f878552d74546b17fd6832510fe6177017c9f99
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-04 12:11:23 +0200

    refactor: change request from openRouteService to google maps

--- src/api/requestCordinates.ts
+++ src/api/requestCordinates.ts
@@ -1,12 +1,14 @@
-import axios from "axios"
+export const requestCordinates = async (destination: string) => {
+  const { Place } = await google.maps.importLibrary("places");
 
+  const request = {
+    textQuery: {destination},
+    fields: ["location", "formattedAddress"],
+    includedType: "", 
+    useStrictTypeFiltering: true,
+  };
 
-export const requestCordinates = async(destination: string) => {
-    try{
-        const response = await axios.get(`https://api.openrouteservice.org/geocode/search?api_key=eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjJiMmQ0ZTllYmNkNDQ2ZWVhYTZhOWE0Yzc5Mzc0N2FhIiwiaCI6Im11cm11cjY0In0=&text=${destination}`)
-        return response.data
-    }   
-    catch(error){
-        console.error(error)
-    }
-}
+  const { places } = await Place.searchByText(request);
+
+  return places[0];
+};
