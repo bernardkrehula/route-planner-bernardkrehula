87f799a61779ffb8ea06fc49d4e6cc7c23fecd96
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-07 14:43:03 +0200

    refactor: change <TestApp> component to <MapWrapper> as main router element

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import MapWrapper from "./components/map/MapWrapper";
-import TestApp from "./TestApp";
 
 const defaultCoords = [46.31, 16.34];
 
 const router = createBrowserRouter([
   {
-    element: <TestApp/>,
+    element: <MapWrapper/>,
     path: "/",
   },
 ]);
