80b5b24de1a7d5a1496454a49375a396009636e6
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-07 14:42:21 +0200

    refactor: change suggestionFilter to return object insted of string

--- src/utils/suggestionFilter.ts
+++ src/utils/suggestionFilter.ts
@@ -2,7 +2,7 @@ export const filterSuggestions = (rawSuggestions) => {
   let suggestions = [];
 
   rawSuggestions.map((suggest) => {
-    const value = suggest.oh.Bi.flatMap((value) => value[2][0]);
+    const value = suggest.oh.Bi.flatMap((value) => ({locationId: value[1], location: value[2][0]}));
     suggestions.push(value);
   });
   return suggestions.flatMap((value) => value);
