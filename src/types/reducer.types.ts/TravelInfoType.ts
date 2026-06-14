ba1cec875e02954cfe0b590b0dbd454da3f2ba2b
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-14 19:08:03 +0200

    refactor: remove unused code and files

--- src/types/reducer.types.ts/TravelInfoType.ts
+++ src/types/reducer.types.ts/TravelInfoType.ts
@@ -1,4 +1,5 @@
 import type { LegInfo } from "./LegInfoType";
+import type { RouteType } from "./RouteType";
 
 export type TravelInfo = {
   legs: LegInfo[];
@@ -16,71 +17,6 @@ export type TravelInfo = {
       lng: number;
     };
   };
-  route: {
-    legs: {
-      distanceMeters: number;
-      duration: string;
-      staticDuration: string;
-      polyline: {
-        encodedPolyline: string;
-      };
-      startLocation: {
-        latLng: {
-          latitude: number;
-          longitude: number;
-        };
-      };
-      endLocation: {
-        latLng: {
-          latitude: number;
-          longitude: number;
-        };
-      };
-      steps: {
-        distanceMeters: number;
-        staticDuration: string;
-        polyline: {
-          encodedPolyline: string;
-        };
-        startLocation: {
-          latLng: {
-            latitude: number;
-            longitude: number;
-          };
-        };
-        endLocation: {
-          latLng: {
-            latitude: number;
-            longitude: number;
-          };
-        };
-        navigationInstruction: {
-          maneuver: string;
-          instructions: string;
-        };
-        localizedValues: {
-          distance: { text: string };
-          staticDuration: { text: string };
-        };
-        travelMode: string;
-      }[];
-      localizedValues: {
-        distance: { text: string };
-        duration: { text: string };
-        staticDuration: { text: string };
-      };
-    }[];
-    viewport: {
-      low: {
-        latitude: number;
-        longitude: number;
-      };
-      high: {
-        latitude: number;
-        longitude: number;
-      };
-    };
-    polylineDetails: Record<string, unknown>;
-  };
+  route: RouteType;
   travelMode: string;
 };
