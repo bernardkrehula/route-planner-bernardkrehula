c04dcbb7883e94b585884cb37c3e44066b386c36
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-14 19:06:33 +0200

    refactor: change RESET case

--- src/components/map/MapWrapper/travelReducer.ts
+++ src/components/map/MapWrapper/travelReducer.ts
@@ -1,11 +1,22 @@
-export const travelReducer = (state: TravelInfo, action: TravelAction): TravelInfo => {
+import type { TravelAction } from "#/types/reducer.types.ts/TravelActionType";
+import type { TravelInfo } from "#/types/reducer.types.ts/TravelInfoType";
+
+export const travelReducer = (
+  state: TravelInfo,
+  action: TravelAction,
+): TravelInfo => {
   switch (action.type) {
     case "SET_TRAVEL_INFO":
       if (JSON.stringify(state.legs) === JSON.stringify(action.payload.legs))
         return state;
       return { ...state, legs: action.payload.legs };
     case "RESET":
-      return { legs: [] };
+      return {
+        legs: [],
+        coordinates: { start: null, stop: null, end: null },
+        route: null,
+        travelMode: "DRIVE",
+      };
     default:
       return state;
   }
