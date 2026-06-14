2815ed03845aa4e33b9d8837bed47735fe2d42aa
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-14 16:46:13 +0200

    refactor: remove unused code

--- src/components/map/MapWrapper/index.tsx
+++ src/components/map/MapWrapper/index.tsx
@@ -1,93 +1,34 @@
 import "./index.css";
-import {
-  AdvancedMarker,
-  APIProvider,
-  Map,
-  Marker,
-  Pin,
-  type MapCameraChangedEvent,
-} from "@vis.gl/react-google-maps";
-import { useNavigate, useSearchParams } from "react-router";
-import { useEffect, useReducer, useState } from "react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { useReducer } from "react";
 import RoutePannel from "../RoutePannel";
-import Route from "#/testFolder/components/route";
-import { RoutesApi } from "#/testFolder/routes-api";
+import Route from "./Route";
+import { travelReducer } from "./travelReducer";
+import { apiClient, appearance, mapOptions } from "./constants/mapConfig";
 
 const ApiKey = import.meta.env.VITE_GOOGLE_MAPS_PLACES_API_KEY;
 
-const mapOptions = {
-  defaultCenter: { lat: 22, lng: 0 },
-  defaultZoom: 3,
-  gestureHandling: "greedy",
-  disableDefaultUI: true,
-};
-const appearance = {
-  walkingPolylineColor: "#000",
-  defaultPolylineColor: "#3189c7",
-  stepMarkerFillColor: "#333333",
-  stepMarkerBorderColor: "#000000",
-};
-
-const routeOptions = {
-  routingPreference: "ROUTING_PREFERENCE_UNSPECIFIED",
-  computeAlternativeRoutes: false,
-  units: "METRIC",
-};
-const API_KEY: string = (globalThis.GOOGLE_MAPS_API_KEY ??
-  import.meta.env.VITE_GOOGLE_MAPS_PLACES_API_KEY) as string;
-const apiClient = new RoutesApi(API_KEY);
-
-type TravelInfo = {
-  duration: string | null;
-  distance: number | null;
-}
-
-type TravelAction = 
-  | { type: "SET_TRAVEL_INFO"; payload: { duration: string; distance: number } }
-  | { type: "RESET" }
-
-const travelReducer = (state: TravelInfo, action: TravelAction): TravelInfo => {
-  switch (action.type) {
-    case "SET_TRAVEL_INFO":
-      if (state.duration === action.payload.duration && 
-          state.distance === action.payload.distance) return state;
-      return { ...state, ...action.payload };
-    case "RESET":
-      return { duration: null, distance: null };
-    default:
-      return state;
-  }
-}
-
 const MapWrapper = () => {
-  const [coordinates, setCoordinates] = useState({
-    start: null,
-    stop: null,
-    end: null,
-  });
-  const [travelMode, setTravelMode] = useState({ travelMode: "DRIVE" });
   const [state, dispatch] = useReducer(travelReducer, {
-    duration: null,
-    distance: null
-  })
-
-  const rawRouteOptions = {...routeOptions, ...travelMode}
+    legs: null,
+    coordinates: {
+      start: null,
+      stop: null,
+      end: null,
+    },
+    route: null,
+    travelMode: "DRIVE",
+  });
 
   return (
     <div className="map-wrapper">
       <APIProvider apiKey={ApiKey}>
-        <RoutePannel
-          setCoordinates={setCoordinates}
-          setTravelMode={setTravelMode}
-          state={state}
-        />
+        <RoutePannel state={state} dispatch={dispatch} />
         <Map className="map" mapId={"d10c35da7e1c69a696d8e796"} {...mapOptions}>
           <Route
             apiClient={apiClient}
-            routeOptions={rawRouteOptions}
             appearance={appearance}
-            {...coordinates}
-            travelMode={travelMode}
+            state={state}
             dispatch={dispatch}
           />
         </Map>
