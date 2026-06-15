b872d9a7bb1efcea4d49970ae0ca321dd4764954
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-15 17:50:16 +0200

    refactor: change passing props to handleDestinationClick function

---
+++ src/components/map/MapWrapper/RoutePannel/FormInput/index.tsx
@@ -0,0 +1,44 @@
+import "./index.css";
+import Input from "#/components/ui/input";
+import type { FormInputType } from "#/types/form.types.ts/FormInputType";
+import ListItem from "./ListItem";
+
+const FormInput = ({
+  name,
+  placeholder,
+  inputValues,
+  activeSearch,
+  handleInputChange,
+  handleDestinationClick,
+  suggestions,
+}: FormInputType) => {
+  return (
+    <form className="form-input">
+      <Input
+        name={name}
+        placeholder={placeholder}
+        value={inputValues[name]}
+        onChange={handleInputChange}
+        variation={activeSearch[name] && "custom-search"}
+      />
+      {activeSearch[name] && (
+        <ul className="destination-options">
+          {suggestions.map((suggestion, key) => {
+            const { locationId, location } = suggestion;
+
+            return (
+              <ListItem
+                key={key}
+                label={location}
+                handleDestinationClick={() =>
+                  handleDestinationClick({...suggestion, inputName: name})
+                }
+              />
+            );
+          })}
+        </ul>
+      )}
+    </form>
+  );
+};
+export default FormInput;
