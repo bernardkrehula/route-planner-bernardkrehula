84aa3c1aefbe2693a85f9fb002abea9614c4cba6
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-15 17:45:58 +0200

    refactor: make space with auto save

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import MapWrapper from "./components/map/MapWrapper";
 
-const defaultCoords = [46.31, 16.34];
 
 const router = createBrowserRouter([
   {
-    element: <MapWrapper/>,
+    element: <MapWrapper />,
     path: "/",
   },
 ]);
