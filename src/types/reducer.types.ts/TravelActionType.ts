cf96bcdf4e40526520cb160a8b6f4ae9a405651e
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-15 17:44:59 +0200

    refactor: add new type to travelAction type

--- src/types/reducer.types.ts/TravelActionType.ts
+++ src/types/reducer.types.ts/TravelActionType.ts
@@ -4,5 +4,6 @@ import type { RouteType } from "./RouteType";
 export type TravelAction =
   | { type: "SET_ROUTE"; payload: RouteType }
   | { type: "SET_COORDS"; payload: {checkpoint: string; coords: {lat: number; lng: number;}}}
+  | { type: "SET_TRAVEL_MODE"; payload: { travelMode: string};}
   | { type: "SET_TRAVEL_INFO"; payload: { legs: LegInfo[] } }
   | { type: "RESET" };
