86be667be6ea547943c14079ff6156e68a8e6665
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-15 17:44:23 +0200

    chore: remove unused code

--- src/components/map/RoutePannel/SelectForm/Option/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { OptionType } from "#/types/ui.types.ts/OptionType";
-
-const Option = ({value, label}: OptionType) => {
-    return(
-        <option value={value}>{label}</option>
-    )
-}
-export default Option;
